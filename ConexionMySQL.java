f30606c8c40f29fc7a4a193bc9b8275a5b725eb4
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-19 23:38:29 -0500

    Inicio del programa

---
+++ ConexionMySQL.java
@@ -0,0 +1,26 @@
+import java.sql.*;
+
+public class ConexionMySQL {
+    //Atributos
+    private String url;
+    private String usuario;
+    private String password;
+    private Connection conexion;
+    private Statement statement;
+    //Constructor
+    public ConexionMySQL(String url, String usuario, String password) {
+        this.url = url;
+        this.usuario = usuario;
+        this.password = password;
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion= DriverManager.getConnection(url, usuario, password);
+            statement = conexion.createStatement();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+    //Metodos
+    public void Cone
+}

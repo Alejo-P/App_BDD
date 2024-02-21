c9315ab8d09e71b599ae734a0c006c59bd33115f
Alejo-P <marcelo.pinzon@epn.edu.ec>
2024-02-21 09:37:25 -0500

    Modificaciones en el estilo

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
     public static void main(String[] args) {
         JFrame Inicio = new JFrame("Manipulacion BDD");
         Inicio.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Inicio.setResizable(false); /*Evitar que se pueda maximizar la ventana principal*/
         Inicio.setLocationRelativeTo(null);
         Inicio.setContentPane(new Ventana_Principal().Ventana_principal);
         Inicio.pack();
